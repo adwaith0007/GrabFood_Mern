bcec0656c80f4d7c8ea4e4f40f273e8bb7b4f568
Adwaith.k <102245239+adwaith0007@users.noreply.github.com>
2024-04-30 13:53:43 +0530

    domain_url

--- client/src/api.ts
+++ client/src/api.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://13.232.169.249/api', 
-  baseURL: 'http://35.154.131.245/api', 
+  // baseURL: 'http://35.154.131.245/api', 
+  baseURL: 'http://www.grabfood.life/api'
   // baseURL: 'http://localhost:5000/api', 
 //   timeout: 5000, 
 //   headers: {
